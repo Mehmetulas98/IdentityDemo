ca36c717ee8a76c0218f6822051629dfff05c071
Mehmetulas98 <44477481+Mehmetulas98@users.noreply.github.com>
2021-10-30 15:30:18 +0300

    Add project files.

---
+++ Data/Migrations/20211019103312_migration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace IdentityDemo.Data.Migrations
+{
+    public partial class migration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Religion",
+                table: "AspNetUsers",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Religion",
+                table: "AspNetUsers");
+        }
+    }
+}
